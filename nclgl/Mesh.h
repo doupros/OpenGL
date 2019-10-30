ab9cde25d0b6d8859812189c819fcba8d05efde5
Hemi Li <b8043765@newcastle.ac.uk>
2019-10-30 16:13:32 +0000

    adding

---
+++ nclgl/Mesh.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "../../NCLGL/OGLRenderer.h"
+
+enum MeshBuffer {
+	VERTEX_BUFFER, COLOUR_BUFFER, MAX_BUFFER
+};
+class Mesh {
+public:
+	Mesh(void);
+	~Mesh(void);
+
+	virtual void Draw();
+	static Mesh* GenerateTriangle();
+
+protected:
+	void BufferData();
+
+	GLuint arrayObject;
+	GLuint bufferObject[MAX_BUFFER];
+	GLuint numVertices;
+	GLuint type;
+
+	Vector3* vertices;
+	Vector4* colours;
+
+};
+
+
